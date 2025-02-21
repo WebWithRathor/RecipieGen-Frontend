62a3f7a4716e230aca286ebc07d7bab127d0bbf4
WebWithRathor <devrajrathor143@gmail.com>
2025-02-21 19:31:23 +0530

    .

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,9 @@ import { Provider } from "react-redux";
 import { store } from "./store/store.jsx";
 
 createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
+  </BrowserRouter>
 );
