db44769a97089e1b54b10d299bf71751aec22ac7
WebWithRathor <devrajrathor143@gmail.com>
2025-02-21 19:17:06 +0530

    .

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -16,13 +16,21 @@ const Recipes = () => {
       <h1 className="text-4xl md:text-5xl font-extrabold text-gray-800 mb-12 text-center">
         Our Recipes
       </h1>
-      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 place-items-center">
+      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-items-center place-items-center">
         {recipes.length > 0 ? (
           recipes.map((card, i) => <Card key={i} recipe={card} />)
         ) : (
-          <p className="text-gray-600 text-lg col-span-full text-center">
-            No recipes found. Please check back later.
-          </p>
+          Array.from({ length: 5 }).map((_, index) => (
+            <div
+              key={index}
+              className="bg-white w-full p-4 rounded-lg shadow-md animate-pulse"
+            >
+              <div className=" h-48 bg-gray-300 rounded-md"></div>
+              <div className="mt-4 h-5  bg-gray-300 rounded"></div>
+              <div className="mt-2 h-4  bg-gray-300 rounded"></div>
+              <div className="mt-2 h-4  bg-gray-300 rounded"></div>
+            </div>
+          ))
         )}
       </div>
     </div>
