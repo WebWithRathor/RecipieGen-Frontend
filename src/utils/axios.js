8c9bedfb76ab4fed500b57734f34470e5170f653
WebWithRathor <devrajrathor143@gmail.com>
2025-02-21 18:50:09 +0530

    .

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_BASE_URI,
   headers: {
     "Content-Type": "application/json",
   },
